04aba3bd3907589fbfda88f7167408940d24197a
ying0704 <yiboxiaowang@gmail.com>
2022-11-25 03:53:04 +1030

    Draft banana and le shy

---
+++ Assets/Scripts/Credit.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Monkey controller = other.GetComponent<Monkey();
+
+        if (controller != null)
+        {
+            
+             Destroy(gameObject);
+            
+        }
+    }
+   
+
+}
