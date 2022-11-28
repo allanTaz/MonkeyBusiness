4d034d6f00f93510b90dc25908615d8d63badc8a
Allan1skku <80329979+allanTaz@users.noreply.github.com>
2022-11-28 20:10:59 +0900

    Update BasicCameraFollow.cs

--- Assets/Scripts/BasicCameraFollow.cs
+++ Assets/Scripts/BasicCameraFollow.cs
@@ -9,10 +9,10 @@ public class BasicCameraFollow : MonoBehaviour
 
 	void Update()
 	{
-		targetPos = new Vector3(followTarget.transform.position.x, transform.position.y, transform.position.z);
-		if (targetPos.x > -118f && targetPos.x < 188f) { 
+		targetPos = new Vector3(followTarget.transform.position.x+5, transform.position.y, transform.position.z);
+		if (targetPos.x > -118f && targetPos.x < 188f) {
 			Vector3 velocity = targetPos - transform.position;
 			transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, 1.0f, moveSpeed * Time.deltaTime);
-		} 
+		}
 	}
 }
