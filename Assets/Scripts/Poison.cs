7332e9526438685aa9181fd1d75b04e5471900df
ying0704 <yiboxiaowang@gmail.com>
2022-12-01 08:48:01 +1030

    A bit update（almost done）

--- Assets/Scripts/Poison.cs
+++ Assets/Scripts/Poison.cs
@@ -3,23 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Poison : MonoBehaviour
-
 {
-    public PlayerScript player;
-   // public HeathBar heathBar;
- 
-    void OnTriggerEnter2D(Collider2D other)
+
+    private int num;
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        PlayerScript player = other.GetComponent<PlayerScript>();
-       // HeathBar heathBar
-        if (player != null)
+
+
+        if (collision.gameObject.tag == "pro")
         {
-            player.health -= 1;
-            player.healthBar.UpdateHealthBar();
-            player.transform.position = player.lastPosition;
-          //  player.animator.SetBool("IsJumping", true);
-          //  player.rb.velocity = new Vector2(player.rb.velocity.x, player.jumpPower);
+            num += 1;
+            if(num==3)
+            {
+                Destroy(gameObject);
+            }
+            
         }
 
     }
+
 }
