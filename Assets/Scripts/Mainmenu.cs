62f8b5c4ff61230b7d248852650bbaa49aa15aa2
ying0704 <yiboxiaowang@gmail.com>
2022-11-30 22:08:19 +1030

    Update（some bugs）

--- Assets/Scripts/Mainmenu.cs
+++ Assets/Scripts/Mainmenu.cs
@@ -1,11 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class Mainmenu : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public AudioMixer audioMixer;
+    public GameObject player;
+
+
+
    public void PlayGame() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -22,5 +26,26 @@ public class Mainmenu : MonoBehaviour
     public void Pause()
     {
         pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
     }
+    public void ReturnGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void SetVolume(float value) 
+    {
+        audioMixer.SetFloat("MainVolume", value);
+    
+    }
+
+    public void Mute()
+    {
+        player.GetComponent<AudioSource>().enabled = false;
+
+    }
+
+
+
 }
