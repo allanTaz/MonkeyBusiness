56937bef205d268c4d3830b02941872fc0c0e55a
ying0704 <yiboxiaowang@gmail.com>
2022-12-01 18:31:38 +1030

    update

--- Assets/Scripts/Enemyhealthbar.cs
+++ Assets/Scripts/Enemyhealthbar.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Enemyhealthbar : MonoBehaviour
 {
+<<<<<<< Updated upstream
 
     
     public P1 Enemy;
@@ -29,4 +30,29 @@ public class Enemyhealthbar : MonoBehaviour
         }
     }
 
+=======
+ 
+
+ //   public E1 Enemy;
+//    public Sprite[] Enemyimage;
+
+ //   public void UpdateEnemy()
+ //   {
+  //      switch (Enemy.Enemyhealth)
+   //     {
+   //         case 3:
+   //             GetComponent<Image>().sprite = Enemyimage[0];
+  //              break;
+   //         case 2:
+   //             GetComponent<Image>().sprite = Enemyimage[1];
+   //             break;
+   //         case 1:
+   //             GetComponent<Image>().sprite = Enemyimage[2];
+    //            break;
+   //         case 0:
+    //           GetComponent<Image>().sprite = Enemyimage[3];
+    //            break;
+    //    }
+  //  }
+>>>>>>> Stashed changes
 }
